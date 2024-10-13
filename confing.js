77f8646baba4c040ddf45bd2196ba0817c4c64fd
Sadew1234564 <sadewsunera2@gmail.com>
2024-10-13 18:24:56 +0530

    Update and rename heroku.yml to confing.js

---
+++ confing.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "9n0AkLoC#tpWKlqccywmUH_5iisAfM196xVAS1VNB_M7p4EweLEc",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+};
